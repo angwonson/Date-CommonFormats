a525b42962c607be8398500ad7e69033a85158e4
Chris Fulton <chris@masonqm.com>
2011-06-03 15:29:33 -0700

    added GIT url to docs

--- lib/Date/CommonFormats.pm
+++ lib/Date/CommonFormats.pm
@@ -246,6 +246,10 @@ You can also look for information at:
 
 =over 4
 
+=item * GITHUB
+
+L<https://github.com/masonqm/Date-CommonFormats>
+
 =item * RT: CPAN's request tracker
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Date-CommonFormats>
